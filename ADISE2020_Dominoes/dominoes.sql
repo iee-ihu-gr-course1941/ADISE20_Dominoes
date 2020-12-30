4995487f8da12e70771d6ee74d68dc1d035bdba0
Elli Eirini Osmantzikidou <elli.osmantzikidou@gmail.com>
2020-12-30 19:39:48 +0200

    Added table state

---
+++ ADISE2020_Dominoes/dominoes.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2020 at 04:44 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dominoes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE `players` (
+  `id` int(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `state`(
+  `gameID` INT(6) PRIMARY KEY AUTO_INCREMENT,
+  `currentState` VARCHAR(65535)
+)
+
+
+--
+-- Dumping data for table `players`
+--
+
+INSERT INTO `players` (`id`, `username`, `password`) VALUES
+(1, 'geo', '123'),
+(2, 'zero', '123456'),
+(3, 'one', '123'),
+(4, 'two', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tiles`
+--
+
+CREATE TABLE `tiles` (
+  `numTiles` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tiles`
+--
+
+INSERT INTO `tiles` (`numTiles`) VALUES
+(0),
+(1),
+(2),
+(3),
+(4),
+(5),
+(6);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uname` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `players`
+--
+ALTER TABLE `players`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+CREATE TABLE State(GameID INT(6) PRIMARY KEY,
+                     CurrentState VARCHAR(65535)
