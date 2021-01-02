4633e91862d197e651fefd74e9ce119d56436906
Elli Eirini Osmantzikidou <elli.osmantzikidou@gmail.com>
2021-01-02 11:52:33 +0200

    Syntax fixes

---
+++ ADISE2020_Dominoes/api.php
@@ -0,0 +1,46 @@
+<?php
+require "domino-function-library.php";
+
+if(!isset($connected)||$connected == false){
+	require "dbconnect.php";
+}
+if (session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+}
+
+	$JSONstate = selectState($_SESSION['gameID']);
+	$state = jsonToState($JSONstate);
+
+if (isset($_GET['button'])) {
+	
+	$json_output = array();
+	if ($_GET['button'] == "start"){
+		$json_output['hand'] = getPlayerHandToJSON($state,$_SESSION['player1']);
+	}
+	else if ($_GET['button'] == "play"){
+		playDomino($state, $_GET['front'] , $_GET['back']);
+		$json_output['board'] = getBoardToJSON($state);
+		$json_output['hand'] = getPlayerHandToJSON($state,$_SESSION['player1']);
+	}
+	else if ($_GET['button'] == "flip"){
+		flipDominoInMyHand($state, $_GET['front'] , $_GET['back'])
+		$json_output['hand'] = getPlayerHandToJSON($state,$_SESSION['player1']);
+	}
+	else if ($_GET['button'] == "draw"){
+		takeFromPile($state)
+		$json_output['hand'] = getPlayerHandToJSON($state,$_SESSION['player1']);
+	}
+	$JSONstate = stateToJSON($state);
+	updateTableFromState($JSONstate,$_SESSION['gameID']);
+	
+	session_write_close();
+
+/*
+	if($state["end"]==True){
+		header('Location:');
+	}
+	*/
+	echo $json_output;
+}
+die;
+?>
