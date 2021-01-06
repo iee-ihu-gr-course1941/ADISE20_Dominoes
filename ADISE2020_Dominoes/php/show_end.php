817ed1b2f55a91df306a283bc4061f7998ddf6e8
geoRagatsi <73553315+geoRagatsi@users.noreply.github.com>
2021-01-06 20:10:21 +0200

    Add files via upload

--- ADISE2020_Dominoes/php/show_end.php
+++ ADISE2020_Dominoes/php/show_end.php
@@ -1,13 +1,17 @@
 <?php
+	
 	if (session_status() !== PHP_SESSION_ACTIVE) {
 		session_start();
 	}
 	if (isset($_SESSION['status'])) {
+		
+		include "reactivate.php"; 
+		
 		if ($_SESSION['status'] == 3) {
 		echo $_SESSION['EndMessage'];
 		
 		?>
-			<form action="start.php" method="POST">					
+			<form action="../waiting.html" method="POST">					
 				<label for="newgame"><button class="button2">Start again</button></label><br/>
 				<input type="submit" name="newgame" style="display:none">
 			</form>
